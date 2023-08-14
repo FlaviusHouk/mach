1380eb21fb2bead8e111ad4bd09d002e057de1d1
Vladyslav Stovmanenko <flaviusglamfenix@gmail.com>
2023-08-14 11:58:45 +0300

    Define page size and move some vm parameters

---
+++ aarch64/aarch64/spl.h
@@ -0,0 +1,4 @@
+#ifndef _AARCH64_SPL_H_
+#define _AARCH64_SPL_H_
+
+#endif //_AARCH64_SPL_H_
--- aarch64/include/mach/aarch64/vm_param.h
+++ aarch64/include/mach/aarch64/vm_param.h
@@ -3,6 +3,27 @@
 
 /* Parameters of the virtual memory  */
 
+/* 
+ * AArch64 can work with 4K or 64K page sizes.
+ * Probably one defines the size here. However
+ * I do not know how to properly work with these values yet. 
+ */
 
+
+#define PAGE_SHIFT 12
+#define TABLE_SHIFT 9
+#define SECTION_SHIFT (PAGE_SHIFT + TABLE_SHIFT)
+
+//4K
+#define PAGE_SIZE (1 << PAGE_SHIFT)
+#define SECTION_SIZE (1 << SECTION_SHIFT)
+
+#define LOW_MEMORY (2 * SECTION_SIZE)
+
+#ifndef __ASSEMBLER__
+
+void memzero(unsigned long src, unsigned int n);
+
+#endif
 
 #endif //_MACHINE_AARCH64_VM_PARAM_H_
